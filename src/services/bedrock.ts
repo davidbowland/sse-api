d087d848b53f8d5253154cad4314188cd40c4e20
David Bowland <dbowland1@gmail.com>
2025-08-29 00:34:12 -0500

    Properly use system prompt

--- src/services/bedrock.ts
+++ src/services/bedrock.ts
@@ -5,6 +5,9 @@ import { log, logDebug } from '../utils/logging'
 
 const runtimeClient = new BedrockRuntimeClient({ region: 'us-east-1' })
 
+const MAX_MESSAGE_HISTORY_COUNT = 25
+const MAX_RECENT_MESSAGE_COUNT = 10
+
 export const invokeModel = async <T = unknown>(
   prompt: Prompt,
   data: string,
@@ -16,17 +19,28 @@ export const invokeModel = async <T = unknown>(
   return invokeModelMessage<T>(promptWithContext, [{ content: data, role: 'user' }])
 }
 
+const getMessageHistory = (history: ChatMessage[]): ChatMessage[] => {
+  if (history.length <= MAX_RECENT_MESSAGE_COUNT) {
+    return history
+  }
+  const recentMessages = history.slice(-MAX_RECENT_MESSAGE_COUNT)
+  const userMessages = history.slice(0, -MAX_RECENT_MESSAGE_COUNT).filter((msg: ChatMessage) => msg.role === 'user')
+  return userMessages.concat(recentMessages).slice(-MAX_MESSAGE_HISTORY_COUNT)
+}
+
 export const invokeModelMessage = async <T = unknown>(
   prompt: Prompt,
   history: ChatMessage[],
   data?: Record<string, unknown>,
 ): Promise<T> => {
-  const messageContent = data ? prompt.contents.replace('${data}', JSON.stringify(data)) : prompt.contents
-  logDebug('Invoking model', { data, messageContent, prompt })
+  const systemContent = data ? prompt.contents.replace('${data}', JSON.stringify(data)) : prompt.contents
+  logDebug('Invoking model', { data, prompt, systemContent })
+
   const messageBody = {
     anthropic_version: prompt.config.anthropicVersion,
     max_tokens: prompt.config.maxTokens,
-    messages: [{ content: messageContent, role: 'user' }, ...history],
+    messages: getMessageHistory(history),
+    system: systemContent,
     temperature: prompt.config.temperature,
     top_k: prompt.config.topK,
   }
@@ -35,6 +49,9 @@ export const invokeModelMessage = async <T = unknown>(
     history2: history.slice(10, 20),
     history3: history.slice(20),
     messageBody,
+    messages1: messageBody.messages.slice(0, 10),
+    messages2: messageBody.messages.slice(10, 20),
+    messages3: messageBody.messages.slice(20),
   })
   const command = new InvokeModelCommand({
     body: new TextEncoder().encode(JSON.stringify(messageBody)), // new Uint8Array(), // e.g. Buffer.from("") or new TextEncoder().encode("")
